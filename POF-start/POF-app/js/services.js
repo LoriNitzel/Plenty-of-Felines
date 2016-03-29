30735c979449817e22221dd6e7a38d25fa615142
Lori Nitzel <lorinitzel@mac.com>
2016-03-29 16:26:11 -0600

    added color to edit cat page

--- POF-start/POF-app/js/services.js
+++ POF-start/POF-app/js/services.js
@@ -1,40 +1,46 @@
+
 angular.module('pofApp')
+
+  .service('backendService', function(){
+    return {url: "https://stark-depths-49375.herokuapp.com"}
+  })
 
 //****** display all cats on allcats.html *****//
 
-  .service('catsService', ['$http', function($http){
+  .service('catsService', ['$http', 'backendService', function($http, backendService){
     return {
       getCats: function(){
-        return $http.get('http://localhost:3000/cats');
+        console.log(backendService.url);
+        return $http.get(backendService.url + '/cats');
       }
     }
   }])
 
 //****** display individual cats on cats/:id *****//
 
-  .service('indcatsService', ['$http', '$routeParams', function($http, $routeParams){
+  .service('indcatsService', ['$http', '$routeParams', 'backendService', function($http, $routeParams, backendService){
     return{
       showCats: function(param1){
-        return $http.get('http://localhost:3000/cats/' + param1);
+        return $http.get(backendService.url + '/cats/' + param1);
       },
       createCat: function(newCatData){
-        return $http.post('http://localhost:3000/cats/admin/create', newCatData);
+        return $http.post(backendService.url, newCatData);
       },
       updateCat: function(param1, updateCatData){
-        return $http.put('http://localhost:3000/cats/admin/' + param1, updateCatData);
+        return $http.put(backendService.url + '/cats' + '/admin/' + param1, updateCatData);
       },
       deleteCat: function(param1){
-        return $http.delete('http://localhost:3000/cats/admin/delete/' + param1);
+        return $http.delete(backendService.url + '/cats/' + param1);
     }
   }
 }])
  
 //****** post Question data entered by Users in Users table*****//
 
-  .service('holdingService', ['$http', function($http){
+  .service('holdingService', ['$http', 'backendService', function($http, backendService){
     return {
       answerdata: function(answers){
-        return $http.put('http://localhost:3000/questions', answers)
+        return $http.put(backendService.url + '/questions', answers)
         .then(function(response){
           console.log(response);
         }, function(error){
@@ -46,10 +52,10 @@ angular.module('pofApp')
 
 //****** post User signup data when they register *****//
   
-.service('signupService', ['$http', function($http){
+.service('signupService', ['$http', 'backendService', function($http, backendService){
    return {
       signup: function(userData){
-        return $http.post('http://localhost:3000/users', userData);
+        return $http.post(backendService.url + '/users', userData);
         // .success(function(userData, status){
         //   console.log(userData, status);
         // })
@@ -60,11 +66,11 @@ angular.module('pofApp')
 //****** Sign registered user in to the application ******//
 
 
-.service('signinService', ['$http', function($http){
+.service('signinService', ['$http', 'backendService', function($http, backendService){
   return {
     state: {isLoggedin: false},
     signin: function(email, password){
-      return $http.post('http://localhost:3000/login', {"email": email, "password": password});
+      return $http.post(backendService.url + '/login', {"email": email, "password": password});
       }
     }
 }])
@@ -72,26 +78,26 @@ angular.module('pofApp')
 
 //****** Grab User ID info for Profile ******//
 
-  .service('profileService', ['$http', '$routeParams', function($http, $routeParams){
+  .service('profileService', ['$http', '$routeParams', 'backendService', function($http, $routeParams, backendService){
     return {
       showUser: function(param1){
-        return $http.get('http://localhost:3000/users/' + param1);
+        return $http.get(backendService.url + '/users/' + param1);
       },
       updateUser: function(param1){
-        return $http.put('http://localhost:3000/users/' + param1);
+        return $http.put(backendService.url + '/users/' + param1);
       },
       deleteUser: function(param1){
-        return $http.delete('http://localhost:3000/users/' + param1);
+        return $http.delete(backendService.url + '/users/' + param1);
       }
     }
 }])
 
 //******** GET Matches from Matches Database and Display on Matches Page ********//
 
-   .service('matchesService', ['$http', function($http){
+   .service('matchesService', ['$http', 'backendService', function($http, backendService){
     return {
       getMatches: function(){
-        return $http.get('http://localhost:3000/users/matches');
+        return $http.get(backendService.url + '/users/matches');
       }
     }
   }]);
